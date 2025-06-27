d9c97d8c601a380cc778346b83ca8567cad669fb
Erick Mora <m.erickpeligro@gmail.com>
2025-06-26 21:05:20 -0600

    #19 javascript

--- Roadmap/19 - ENUMERACIONES/javascript/septarian.js
+++ Roadmap/19 - ENUMERACIONES/javascript/septarian.js
@@ -63,4 +63,74 @@ function getDay(day){
 getDay(5)
 
 //EXTRA
-   console.log('//////////////////////Extra////////////////////');
+console.log('//////////////////////Extra////////////////////');
+
+const OrdenStatus = {
+    PENDIENTE: Symbol(1),
+    ENVIADO: Symbol(2),
+    ENTREGADO: Symbol(3),
+    CANCELADO: Symbol(4),
+}
+class Pedido{
+    constructor(id){
+        this.id = id
+        this.estatus = OrdenStatus.PENDIENTE
+    }
+    
+    enviar(){
+        if(this.estatus === OrdenStatus.PENDIENTE){
+            this.estatus = OrdenStatus.ENVIADO
+            console.log("Se a enviado el producto ",this.id)
+        }else{
+            console.log("No se puede enviar, probablemente ya fue enviado o cancelado")
+        }
+    }
+    entregar(){
+        if(this.estatus === OrdenStatus.ENVIADO){
+            this.estatus = OrdenStatus.ENTREGADO
+            console.log("El pedido se a entregado con exito")
+        }else{
+            console.log("No se pudo entregar, revisa el estado del envio")
+        }
+    }
+    cancelar(){
+        if(this.estatus !== OrdenStatus.ENTREGADO){
+            this.estatus = OrdenStatus.CANCELADO
+            console.log("El pedido se a cancelado")
+        }else{
+            console.log("No se pudo cancelar, puede que ya haya sido entregado")
+        }
+    }
+    mostrar(){
+        switch(this.estatus){
+            case OrdenStatus.PENDIENTE:
+                console.log("El pedido ",this.id," esta pendiente",)
+                break
+            case OrdenStatus.ENVIADO:
+                console.log("El pedido ",this.id," a sido enviado",)
+                break
+            case OrdenStatus.ENTREGADO:
+                console.log("El pedido ",this.id," a sido entregado",)
+                break
+            case OrdenStatus.CANCELADO:
+                console.log("El pedido ",this.id," a sido cancelado",)
+                break
+        }
+    }
+
+}
+
+let comida = new Pedido(1)
+let lavadora = new Pedido(2)
+comida.enviar()
+comida.entregar()
+comida.cancelar()
+comida.mostrar()
+console.log("/////////////////////////////////////////////////////////")
+lavadora.entregar()
+lavadora.mostrar()
+lavadora.enviar()
+lavadora.mostrar()
+lavadora.cancelar()
+lavadora.entregar()
+lavadora.mostrar()
