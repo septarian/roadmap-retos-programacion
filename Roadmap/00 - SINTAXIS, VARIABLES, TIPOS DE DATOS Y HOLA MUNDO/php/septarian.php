dc35b585481884b789f109d526be0110e0c85596
Erick Mora <m.erickpeligrogmail.com>
2025-07-05 13:34:15 -0600

    01 avance

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/septarian.php
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/septarian.php
@@ -1,2 +1,9 @@
+<?php
 //SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
 
+    echo "esto es una prueba\n"; //esto es un comentario de una sola linea estilo c++
+    /*
+    esto es una comentario de varias lineas
+    */
+    echo "otra prueba\n";  #este es un comentario de una sola linea estilo shell
+?>
